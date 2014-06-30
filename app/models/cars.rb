ba82f3e551ef43695fafa467e55a41d3b199f571
c13iscuit <barnett.chri@gmail.com>
2014-06-30 06:24:21 -0400

    Added models for manufacturer and car

---
+++ app/models/cars.rb
@@ -0,0 +1,8 @@
+class Cars < ActiveRecord::Base
+  belongs_to :manufacturer
+
+  validates :color, presence: true
+  validates :year, presence: true
+  validates :mileage, presence: true
+  validates :manufacturer_id, presence: true
+end
